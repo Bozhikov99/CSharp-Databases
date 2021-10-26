4996f1a486c6262952b075aa640e3fa9f90c4945
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-10-26 16:24:04 +0300

    Adding exercise.

---
+++ 01. ADO.NET/AllVillainNames/Program.cs
@@ -0,0 +1,254 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace AllVillainNames
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(Configuration.ConnectionString);
+            await connection.OpenAsync();
+
+            await using (connection)
+            {
+                await IncreaseMinionAge(connection);
+            }
+        }
+
+        //01. Villain Names
+        private static async Task VillainNamesAsync(SqlConnection connection)
+        {
+            SqlCommand cmd = new SqlCommand(Queries.GetVilliansWithMoreThan3Minions, connection);
+            SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            await using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                }
+            }
+        }
+
+        //02. Minion Names
+        private static async Task MinionNames(SqlConnection connection, int Id)
+        {
+            SqlCommand getVillainFromId = new SqlCommand(Queries.GetVillainForId, connection);
+            getVillainFromId.Parameters.AddWithValue("Id", Id);
+
+            object villain = await getVillainFromId.ExecuteScalarAsync();
+
+            if (villain == null)
+            {
+                Console.WriteLine($"No villain with ID {Id} exists in the database.");
+                return;
+            }
+
+            Console.WriteLine($"Villain name: {villain}");
+
+            SqlCommand getMinionsForVillain = new SqlCommand(Queries.GetMinionNamesForVillain, connection);
+            getMinionsForVillain.Parameters.AddWithValue("Id", Id);
+
+            SqlDataReader reader = await getMinionsForVillain.ExecuteReaderAsync();
+
+            await using (reader)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["RowNum"]}. {reader["Name"]} {reader["Age"]}");
+                }
+            }
+        }
+        //03. Add Minion
+        private static async Task AddMinion(SqlConnection connection)
+        {
+            //Minion: Bob 14 Berlin
+            //Villain: Gru
+            string[] minionData = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string villainName = Console.ReadLine()
+                .Substring(9);
+
+            string minionName = minionData[1];
+            int minionAge = int.Parse(minionData[2]);
+            string minionTown = minionData[3];
+
+            SqlCommand getTownByName = new SqlCommand(Queries.GetTownFromTownName, connection);
+            getTownByName.Parameters.AddWithValue("@townName", minionTown);
+            object townId = await getTownByName.ExecuteScalarAsync();
+
+            if (townId == null)
+            {
+                SqlCommand createTown = new SqlCommand(Queries.CreateTown, connection);
+                createTown.Parameters.AddWithValue("@townName", minionTown);
+                await createTown.ExecuteNonQueryAsync();
+
+                Console.WriteLine($"Town {minionTown} was added to the database.");
+                townId = await getTownByName.ExecuteScalarAsync();
+            }
+
+
+            SqlCommand createMinion = new SqlCommand(Queries.CreateMinion, connection);
+            createMinion.Parameters.AddWithValue("@nam", minionName);
+            createMinion.Parameters.AddWithValue("@age", minionAge);
+            createMinion.Parameters.AddWithValue("@townId", townId);
+            await createMinion.ExecuteNonQueryAsync();
+
+            SqlCommand getMinionId = new SqlCommand(Queries.GetMinionIdFromName, connection);
+            getMinionId.Parameters.AddWithValue("@Name", minionName);
+            object minionId = await getMinionId.ExecuteScalarAsync();
+
+            SqlCommand getVillain = new SqlCommand(Queries.GetVillianIdFromName, connection);
+            getVillain.Parameters.AddWithValue("@Name", villainName);
+            object villain = await getVillain.ExecuteScalarAsync();
+
+            if (villain == null)
+            {
+                SqlCommand createVillain = new SqlCommand(Queries.CreateVillain, connection);
+                createVillain.Parameters.AddWithValue("@villainName", villainName);
+                await createVillain.ExecuteNonQueryAsync();
+
+                Console.WriteLine($"Villain {villainName} was added to the database.");
+                villain = await getVillain.ExecuteScalarAsync();
+            }
+
+            SqlCommand mapMinion = new SqlCommand(Queries.MapMinionToVillain, connection);
+            mapMinion.Parameters.AddWithValue("@minionId", minionId);
+            mapMinion.Parameters.AddWithValue("@villainId", villain);
+
+            Console.WriteLine($"Successfully added {minionName} to be minion of {villainName}.");
+        }
+
+        private static async Task ChangeTownsNameCasing(SqlConnection connection)
+        {
+            string country = Console.ReadLine();
+
+            SqlCommand changeTownsCasing = new SqlCommand(Queries.SetTownsToUpper, connection);
+            changeTownsCasing.Parameters.AddWithValue("@countryName", country);
+            int rowsAffected = await changeTownsCasing.ExecuteNonQueryAsync();
+
+            if (rowsAffected == 0)
+            {
+                Console.WriteLine("No town names were affected.");
+                return;
+            }
+
+            Console.WriteLine($"{rowsAffected} town names were affected.");
+
+            SqlCommand getTowns = new SqlCommand(Queries.GetAffectedTowns, connection);
+            getTowns.Parameters.AddWithValue("@countryName", country);
+            SqlDataReader reader = await getTowns.ExecuteReaderAsync();
+
+            await using (reader)
+            {
+                List<string> townNames = new List<string>();
+
+                while (reader.Read())
+                {
+                    townNames.Add((string)reader["Name"]);
+                }
+
+                Console.WriteLine($"[{string.Join(", ", townNames)}]");
+            }
+
+        }
+        //05. Remove villain
+        private static async Task DeleteVillain(SqlConnection connection)
+        {
+            int villainId = int.Parse(Console.ReadLine());
+
+            SqlCommand getVillainNameFromId = new SqlCommand(Queries.GetVillainNameFromId, connection);
+            getVillainNameFromId.Parameters.AddWithValue("@villainId", villainId);
+
+            object villainName = await getVillainNameFromId.ExecuteScalarAsync();
+
+            if (villainName==null)
+            {
+                Console.WriteLine("No such villain was found.");
+                return;
+            }
+
+            SqlCommand getMinionCount = new SqlCommand(Queries.GetReleasedMinionsCount, connection);
+            getMinionCount.Parameters.AddWithValue("villainId", villainId);
+            object minionCount = await getMinionCount.ExecuteScalarAsync();
+
+            SqlCommand removeMapping = new SqlCommand(Queries.RemoveVillainMinionMapping, connection);
+            removeMapping.Parameters.AddWithValue("@villainId", villainId);
+            await removeMapping.ExecuteNonQueryAsync();
+
+            SqlCommand deleteVillain = new SqlCommand(Queries.DeleteVillainWithId, connection);
+            deleteVillain.Parameters.AddWithValue("villainId", villainId);
+            await deleteVillain.ExecuteNonQueryAsync();
+
+            Console.WriteLine($"{villainName} was deleted.");
+            Console.WriteLine($"{minionCount} minions were released.");
+        }
+        //6. Print All Minion Names
+        private static async Task GetMinions(SqlConnection connection)
+        {
+            SqlCommand getMinions = new SqlCommand(Queries.GetAllMinions, connection);
+
+            SqlDataReader reader = await getMinions.ExecuteReaderAsync();
+            List<string> minionNames = new List<string>();
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    minionNames.Add((string)reader["Name"]);
+                }
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < minionNames.Count; i++)
+            {
+                Console.WriteLine($"{minionNames[i]}");
+                count++;
+
+                if (count>=minionNames.Count)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{minionNames[minionNames.Count-i-1]}");
+                count++;
+            }
+        }
+
+        //7. Increase Minion Age
+        private static async Task IncreaseMinionAge(SqlConnection connection)
+        {
+            int[] ids = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (int i in ids)
+            {
+                SqlCommand increaseMinionAge = new SqlCommand(Queries.IncreaseMinionAge, connection);
+                increaseMinionAge.Parameters.AddWithValue("Id", i);
+                await increaseMinionAge.ExecuteNonQueryAsync();
+            }
+
+            SqlDataReader reader = await new SqlCommand(Queries.GetMinionNameAge, connection).ExecuteReaderAsync();
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["Name"]} {reader["Age"]}");
+                }
+            }
+        }
+    }
+}
---
+++ 01. ADO.NET/AllVillainNames/Queries.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllVillainNames
+{
+    public class Queries
+    {
+        public const string GetVilliansWithMoreThan3Minions =
+            @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+               FROM Villains AS v
+               JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+           GROUP BY v.Id, v.Name
+             HAVING COUNT(mv.VillainId) > 3 
+           ORDER BY COUNT(mv.VillainId)";
+
+        public const string GetVillainForId =
+            @"SELECT Name FROM Villains WHERE Id = @Id";
+
+        public const string GetMinionNamesForVillain =
+            @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                FROM MinionsVillains AS mv
+                JOIN Minions As m ON mv.MinionId = m.Id
+               WHERE mv.VillainId = @Id
+               ORDER BY m.Name";
+
+        public const string GetVillianIdFromName =
+            @"SELECT Id FROM Villains WHERE Name = @Name";
+
+        public const string GetVillainNameFromId =
+            @"SELECT Name FROM Villains WHERE Id = @villainId";
+
+        public const string RemoveVillainMinionMapping =
+            @"DELETE 
+              FROM MinionsVillains
+             WHERE VillainId=@villainId";
+
+        public const string DeleteVillainWithId =
+            @"DELETE 
+              FROM Villains
+             WHERE Id=@villainId";
+
+        public const string GetReleasedMinionsCount =
+            @" SELECT COUNT(MinionId) AS MinionCount
+    FROM MinionsVillains
+   WHERE VillainId=@villainId
+GROUP BY VillainId";
+
+        public const string GetMinionIdFromName =
+            @"SELECT Id FROM Villains WHERE Name = @Name";
+
+        public const string MapMinionToVillain =
+            @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (@minionId, @villainId)";
+
+        public const string CreateVillain =
+            @"INSERT INTO Villains (Name, EvilnessFactorId)  VALUES (@villainName, 4)";
+
+        public const string GetTownFromTownName =
+            @"SELECT Id FROM Towns WHERE Name = @townName";
+
+        public const string CreateTown =
+            @"INSERT INTO Towns (Name) VALUES (@townName)";
+
+        public const string CreateMinion =
+            @"INSERT INTO Minions (Name, Age, TownId) VALUES (@nam, @age, @townId)";
+
+        public const string SetTownsToUpper =
+            @"UPDATE Towns
+   SET Name = UPPER(Name)
+ WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+        public const string GetAffectedTowns =
+            @" SELECT t.Name 
+   FROM Towns as t
+   JOIN Countries AS c ON c.Id = t.CountryCode
+  WHERE c.Name = @countryName";
+
+        public const string GetAllMinions =
+            @"SELECT Name FROM Minions";
+
+        public const string IncreaseMinionAge =
+            @" UPDATE Minions
+                  SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                WHERE Id = @Id";
+
+        public const string GetMinionNameAge =
+            @"SELECT Name, Age FROM Minions";
+    }
+}
