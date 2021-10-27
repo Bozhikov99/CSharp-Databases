e82aa24b9e3e6c9f9de2ada8d54d4163b4858956
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-10-27 17:47:24 +0300

    Adding exercise.

---
+++ 03. Entity Framework Introduction/13. Find Employees by First Name Starting With Sa/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = GetEmployeesByFirstNameStartingWithSa(context);
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            context
+                .Employees
+                .Where(e => e.FirstName
+                    .ToLower()
+                    .Substring(0, 2) == "sa")
+                    .OrderBy(e=>e.FirstName)
+                    .ThenBy(e=>e.LastName)
+                    .ToList()
+                    .ForEach(e=>sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle} - (${e.Salary:F2})"));
+
+            //var departments = context
+            //    .Departments
+            //    .Where(d => d.Employees.Count > 5)
+            //    .Select(d => new
+            //    {
+            //        d.Name,
+            //        firstName = d.Manager
+            //        .FirstName,
+            //        lastName = d.Manager
+            //        .LastName,
+            //        d.Manager,
+            //        d.Employees
+            //    })
+            //    .ToList();
+
+            //foreach (var d in departments)
+            //{
+            //    sb.AppendLine($"{d.Name} - {d.firstName} {d.lastName}");
+
+            //    foreach (var e in d.Employees
+            //        .OrderBy(em=>em.FirstName)
+            //        .ThenBy(em=>em.LastName))
+            //    {
+            //        sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+            //    }
+            //}
+
+            return sb
+                .ToString();
+
+        }
+    }
+}
