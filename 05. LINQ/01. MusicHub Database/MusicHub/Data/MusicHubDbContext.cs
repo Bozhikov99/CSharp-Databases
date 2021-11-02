457525037cb59554810fed7a6e2965e85002b4f7
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-02 12:24:13 +0200

    Adding exercise.

---
+++ 05. LINQ/01. MusicHub Database/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,43 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public MusicHubDbContext()
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SongPerformer>(sp =>
+            {
+                sp.HasKey(e => new { e.SongId, e.PerformerId });
+            });
+        }
+    }
+}
