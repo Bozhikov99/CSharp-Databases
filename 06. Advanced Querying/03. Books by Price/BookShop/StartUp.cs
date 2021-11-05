3029cf483b4c207f8afb1e49327697994f6e2449
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-05 19:50:25 +0200

    Adding exercise.

---
+++ 06. Advanced Querying/03. Books by Price/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Castle.DynamicProxy.Generators;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+                Console.WriteLine(GetBooksByPrice(db));
+            }
+        }
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            context
+                .Books
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price
+                })
+                .ToList()
+                .ForEach(b => output.AppendLine($"{b.Title} - ${b.Price:F2}"));
+
+            return output
+                .ToString()
+                .Trim();
+        }
+    }
+}
