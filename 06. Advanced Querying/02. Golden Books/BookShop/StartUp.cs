79b07f964adc64ce9bd4a70e6429eba56bd24245
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-05 19:41:45 +0200

    Adding exercise.

---
+++ 06. Advanced Querying/02. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Castle.DynamicProxy.Generators;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+                string cmd = Console.ReadLine();
+                Console.WriteLine(GetGoldenBooks(db));
+            }
+        }
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            context
+                .Books
+                .Where(b => (int)b.EditionType == 2&&b.Copies<5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList()
+                .ForEach(b => output.AppendLine(b));
+
+            return output
+                .ToString()
+                .Trim();
+        }
+    }
+}
