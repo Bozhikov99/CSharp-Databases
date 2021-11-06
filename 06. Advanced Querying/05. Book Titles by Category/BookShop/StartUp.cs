c7791296b2dab3ae42bdf5c88b41e41fc3fd2fbd
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-06 12:35:35 +0200

    Adding exercise.

---
+++ 06. Advanced Querying/05. Book Titles by Category/BookShop/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Castle.DynamicProxy.Generators;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+                string input = Console.ReadLine();
+
+                Console.WriteLine(GetBooksByCategory(db, input));
+            }
+        }
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            StringBuilder output = new StringBuilder();
+            string[] categories = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(c => c.ToLower())
+                    .ToArray();
+
+            context
+                .Books
+                .OrderBy(b => b.Title)
+                .Where(x => x.BookCategories
+                    .Any(bc => categories.Contains(bc.Category.Name.ToLower())))
+                .Select(b => b.Title)
+                .ToList()
+                .ForEach(b => output.AppendLine(b));
+
+            return output
+                .ToString()
+                .TrimEnd();
+        }
+    }
+}
