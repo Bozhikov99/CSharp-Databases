ba552f14911a74b0e06352dada011844ad0ac99e
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-07 19:39:12 +0200

    Adding exercise.

---
+++ 06. Advanced Querying/12. Profit by Category/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Castle.DynamicProxy.Generators;
+    using Data;
+    using Initializer;
+    using Remotion.Linq.Parsing.Structure.IntermediateModel;
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+
+                Console.WriteLine(GetTotalProfitByCategory(db));
+                Console.WriteLine(GetTotalProfitByCategory(db).Length);
+            }
+        }
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var categories = context
+                .Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    Profit = c.CategoryBooks
+                        .Sum(cb => cb.Book.Copies * cb.Book.Price)
+                })
+                .OrderByDescending(c => c.Profit)
+                .ThenBy(c => c.Name)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, categories.Select(c => $"{c.Name} ${c.Profit:F2}"));
+        }
+    }
+}
