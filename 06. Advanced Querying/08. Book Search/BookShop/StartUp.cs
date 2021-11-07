f1e1c3bcfbed6f324a94ced0251c99c6eb612c87
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-07 17:08:26 +0200

    Adding exercise.

---
+++ 06. Advanced Querying/08. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Castle.DynamicProxy.Generators;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+                string input = Console.ReadLine();
+
+                string result = GetBookTitlesContaining(db, input);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+//            StringBuilder output = new StringBuilder();
+
+            return string.Join(Environment.NewLine, context
+                  .Books
+                  .OrderBy(b => b.Title)
+                  .Where(b => b.Title
+                      .ToLower()
+                      .Contains(input.ToLower()))
+                  .Select(b => b.Title)
+                  .ToList());
+
+//            return output
+//                .ToString()
+//                .TrimEnd();
+        }
+    }
+}
