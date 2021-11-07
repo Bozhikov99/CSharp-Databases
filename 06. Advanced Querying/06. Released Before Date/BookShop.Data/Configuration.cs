cba44fb431eae14ab67fbf75c4b1bb07bbb382e6
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-07 16:04:53 +0200

    Adding exercise.

---
+++ 06. Advanced Querying/06. Released Before Date/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString => "Server=DESKTOP-P8F7PSV\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ 06. Advanced Querying/06. Released Before Date/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Castle.DynamicProxy.Generators;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+                string input = Console.ReadLine();
+
+                Console.WriteLine(GetBooksReleasedBefore(db, input));
+            }
+        }
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            StringBuilder output = new StringBuilder();
+            DateTime dateTime = DateTime.ParseExact(date, "dd-MM-yyyy", null);
+
+            context
+                .Books
+                .OrderByDescending(b => b.ReleaseDate)
+                .Where(b => b.ReleaseDate < dateTime)
+                .Select(b => $"{b.Title} - {b.EditionType} - ${b.Price:F2}")
+                .ToList()
+                .ForEach(b => output.AppendLine(b));
+
+            return output
+                .ToString()
+                .TrimEnd();
+        }
+    }
+}
