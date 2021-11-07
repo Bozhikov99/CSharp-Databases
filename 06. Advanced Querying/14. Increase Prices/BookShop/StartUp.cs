1b7313770df3aa28b458318aa93869c7132353e1
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-07 18:30:35 +0200

    Adding exercise.

---
+++ 06. Advanced Querying/14. Increase Prices/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Castle.DynamicProxy.Generators;
+    using Data;
+    using Initializer;
+    using Remotion.Linq.Parsing.Structure.IntermediateModel;
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+            }
+        }
+        public static void IncreasePrices(BookShopContext context)
+        {
+            IQueryable<Book> books = context
+                .Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010);
+
+            foreach (Book book in books)
+            {
+                book.Price += 5;
+            }
+        }
+    }
+}
