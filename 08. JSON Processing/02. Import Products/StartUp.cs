a1bc4216479877bded7cddcb6971baec4dcec049
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-12 18:24:27 +0200

    Adding exercise.

---
+++ 08. JSON Processing/02. Import Products/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+
+            using (dbContext)
+            {
+                dbContext.Database
+                    .EnsureCreated();
+
+
+                Console.WriteLine("Database Created!");
+
+                string inputJson = File.ReadAllText("../../../Datasets/products.json");
+
+                string result = ImportProducts(dbContext, inputJson);
+                Console.WriteLine(result);
+
+                dbContext.Database.EnsureDeleted();
+            }
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            Product[] importedProducts = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.Products.AddRange(importedProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {importedProducts.Length}";
+        }
+    }
+}
