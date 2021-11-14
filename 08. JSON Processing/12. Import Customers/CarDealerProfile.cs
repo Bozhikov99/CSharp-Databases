0c96f899bd259472fcb0546c6e3fb0471b697f6d
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-14 21:03:39 +0200

    Adding exercise.

---
+++ 08. JSON Processing/12. Import Customers/CarDealerProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<SupplierInputDTO, Supplier>();
+
+            CreateMap<PartInputDTO, Part>();
+
+            CreateMap<CustomerDTO, Customer>();
+        }
+    }
+}
---
+++ 08. JSON Processing/12. Import Customers/StartUp.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+
+        public static void Main(string[] args)
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+
+            using (dbContext)
+            {
+                dbContext.Database.EnsureCreated();
+                Console.WriteLine("Database created successfully!");
+
+                string suppliersInput = File.ReadAllText("../../../Datasets/suppliers.json");
+                string partsInput = File.ReadAllText("../../../Datasets/parts.json");
+                string carsInput = File.ReadAllText("../../../Datasets/cars.json");
+                string customersInput = File.ReadAllText("../../../Datasets/customers.json");
+
+                try
+                {
+                    Console.WriteLine(ImportSuppliers(dbContext, suppliersInput));
+                    Console.WriteLine(ImportParts(dbContext, partsInput));
+                    Console.WriteLine(ImportCars(dbContext, carsInput));
+                    Console.WriteLine(ImportCustomers(dbContext, customersInput));
+                }
+                catch (Exception ex)
+                {
+                    dbContext.Database.EnsureDeleted();
+                    Console.WriteLine(ex.Message);
+                    dbContext.Database.EnsureDeleted();
+                    throw;
+                }
+
+                dbContext.Database.EnsureDeleted();
+            }
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = GetMapper();
+
+            IEnumerable<SupplierInputDTO> supplierDtos = JsonConvert.DeserializeObject<IEnumerable<SupplierInputDTO>>(inputJson);
+            IEnumerable<Supplier> suppliers = mapper.Map<IEnumerable<Supplier>>(supplierDtos);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = GetMapper();
+
+            int[] supplierIds = context.Suppliers
+                .Select(s => s.Id)
+                .ToArray();
+
+            IEnumerable<PartInputDTO> partDtos = JsonConvert.DeserializeObject<IEnumerable<PartInputDTO>>(inputJson)
+                .Where(dto => supplierIds.Contains(dto.SupplierId));
+
+            IEnumerable<Part> parts = mapper.Map<IEnumerable<Part>>(partDtos);
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count()}."; ;
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = GetMapper();
+
+            IEnumerable<CarInputDTO> carDtos = JsonConvert.DeserializeObject<IEnumerable<CarInputDTO>>(inputJson);
+            List<Car> cars = new List<Car>();
+
+            foreach (var c in carDtos)
+            {
+                Car current = new Car
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TravelledDistance = c.TravelledDistance
+                };
+
+                foreach (var partId in c?.PartsId.Distinct())
+                {
+                    current.PartCars.Add(new PartCar
+                    {
+                        PartId = partId
+                    });
+                }
+
+                cars.Add(current);
+            }
+
+            context.AddRange(cars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count()}.";
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = GetMapper();
+            IEnumerable<CustomerDTO> customerDTOs = JsonConvert.DeserializeObject<IEnumerable<CustomerDTO>>(inputJson);
+            IEnumerable<Customer> customers = mapper.Map<IEnumerable<Customer>>(customerDTOs);
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Count()}.";
+        }
+
+        private static IMapper GetMapper()
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+            return new Mapper(config);
+        }
+    }
+}
