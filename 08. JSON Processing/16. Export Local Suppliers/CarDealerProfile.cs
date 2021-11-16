9c2c54342658ecbbcf2388645a7db98e8c6afa70
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-16 10:46:40 +0200

    Adding exercise.

---
+++ 08. JSON Processing/16. Export Local Suppliers/CarDealerProfile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO;
+using CarDealer.DTO.Output;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<SupplierInputDTO, Supplier>();
+
+            CreateMap<PartInputDTO, Part>();
+
+            CreateMap<CustomerDTO, Customer>();
+
+            CreateMap<SalesDTO, Sale>();
+
+            CreateMap<Customer, OrderedCustomersOutputDTO>()
+                .ForMember(d => d.BirthDate, y => y.MapFrom(s => s.BirthDate.ToString("dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)));
+
+            CreateMap<Car, ToyotaOutputDTO>();
+
+            CreateMap<Supplier, LocalSuppliersOutputDTO>()
+                .ForMember(d => d.PartsCount, y => y.MapFrom(s => s.Parts.Count));
+        }
+    }
+}
