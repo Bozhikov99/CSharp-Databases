3cd26356dcea54a6bf5f563cf3843edb6d6288fa
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-16 10:23:04 +0200

    Adding exercise.

---
+++ 08. JSON Processing/14. Export Ordered Customers/CarDealerProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<SupplierInputDTO, Supplier>();
+
+            CreateMap<PartInputDTO, Part>();
+
+            CreateMap<CustomerDTO, Customer>();
+
+            CreateMap<SalesDTO, Sale>();
+
+            CreateMap<Customer, OrderedCustomersOutputDTO>()
+                .ForMember(d => d.BirthDate, y => y.MapFrom(s => s.BirthDate.ToString("dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)));
+        }
+    }
+}
