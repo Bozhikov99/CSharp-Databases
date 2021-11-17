a4d5cbe6827017361c2cb645c030683c2652b40a
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-17 17:42:52 +0200

    Adding exercise.

---
+++ 08. JSON Processing/17. Export Cars with Parts/DTO/Output/CustomersSalesOutputDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO.Output
+{
+    public class CustomersSalesOutputDTO
+    {
+        public string FullName { get; set; }
+        public int BoughtCars { get; set; }
+        public decimal SpentMoney { get; set; }
+    }
+}
