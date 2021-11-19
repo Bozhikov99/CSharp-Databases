6e6998c6b70a616073c004a941d51f328dc90c06
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-19 16:00:06 +0200

    Adding exercise.

---
+++ 09. XML Processing/07. Export Categories By Products Count/ProductShopProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using ProductShop.Dtos.Export;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System.Linq;
+using System.Xml;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<UserInputDTO, User>();
+
+            CreateMap<ProductInputDTO, Product>();
+
+            CreateMap<CategoriesInputDTO, Category>();
+
+            CreateMap<CategoriesProductsInputDTO, CategoryProduct>();
+
+            CreateMap<Product, ProductInRangeOutputDTO>()
+                .ForMember(x => x.Buyer, y => y.MapFrom(s => $"{s.Buyer.FirstName} {s.Buyer.LastName}"))
+                .ForMember(x => x.Price, y => y.MapFrom(s => $"{s.Price:F2}"));
+
+            CreateMap<Product, UserProductsProductOutputDTo>();
+
+            CreateMap<User, UserProductsOutputDTO>()
+                .ForMember(x => x.SoldProducts, y => y.MapFrom(s => s.ProductsSold));
+
+            CreateMap<Category, CategoriesByCountOutputDTO>()
+                .ForMember(x => x.Count, y => y.MapFrom(s => s.CategoryProducts.Select(cp => cp.Product).Count()))
+                .ForMember(x => x.AveragePrice, y => y.MapFrom(s => s.CategoryProducts.Select(cp => cp.Product).Average(p => p.Price)))
+                .ForMember(x => x.TotalRevenue, y => y.MapFrom(s => s.CategoryProducts.Select(cp => cp.Product.Price).Sum()));
+        }
+    }
+}
