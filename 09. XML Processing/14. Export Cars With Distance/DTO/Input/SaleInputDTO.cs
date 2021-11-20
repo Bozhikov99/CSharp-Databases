ac8df71c49068192bb285a340bbff496ac489ed1
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-11-20 22:23:06 +0200

    Adding exercise.

---
+++ 09. XML Processing/14. Export Cars With Distance/DTO/Input/SaleInputDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO
+{
+    [XmlType("Sale")]
+    public class SaleInputDTO
+    {
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+
+        [XmlElement("carId")]
+        public int CarId { get; set; }
+
+        [XmlElement("customerId")]
+        public int CustomerId { get; set; }
+    }
+}
